707dccc8d9e4583e7add816311e0ffcc10e1fb47
nc2 <circlethewagons21@protonmail.com>
2022-08-19 00:34:11 -0400

    Add modals for hint, description, message

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ main_buttons = html.Div([dbc.Button("Navigate", className="button-style"), dbc.B
 
 hint_message = html.Div(
     [
-    html.Button("Hint", className="halfscreen-buttons"),
-    html.Button("Message", className="halfscreen-buttons")
+    html.Button("Hint", className="halfscreen-buttons", id="hint-button"),
+    html.Button("Message", className="halfscreen-buttons", id="message-button")
     ]
 )
 
@@ -41,12 +41,79 @@ description = html.Button(
             html.P("Bartholdi Park was named after French Sculptor...", className="button-subtitle")
         ], className="stack")
     ],
-    className="fullscreen long-button flex", style={"margin-top":"20px"}
+    className="fullscreen long-button flex", style={"margin-top":"20px"},
+    id="description-button"
 )
 
 cache_bottom_path = 'assets/cache_bottom.png'
 cache_bottom = html.Img(src=cache_bottom_path, className="fullscreen")
 
+hint_modal = dbc.Modal(
+    [
+        dbc.ModalHeader(html.H2("Hint")),
+        dbc.ModalBody("If you get weary, take a seat and rest for a minute.", style={"white-space": "pre-line"})
+    ],
+    is_open=False,
+    id="hint-modal"
+)
+
+message_modal = dbc.Modal(
+    [
+        dbc.ModalHeader(html.H2("Messages")),
+        dbc.ModalBody("Messages unavailable at this time. Please try again later.", style={"white-space": "pre-line"})
+    ],
+    is_open=False,
+    id="message-modal"
+)
+
+description_modal = dbc.Modal(
+    [
+        dbc.ModalHeader(html.H2("Description")),
+        dbc.ModalBody(
+            """
+                Bartholdi Park was named after French Sculptor Frédéric Auguste Bartholdi who designed the fountain that sits in the middle of the park. The United States government bought the fountain from Bartholdi for $6,000 in 1877, a few years before Bartholdi designed his most famous work, the Statue of Liberty.
+                
+                The park was renovated in 2016 as a Sustainable SITES Initiative project, which emphasizes sustainable gardening in 5 key areas: water, plants, soil, materials, and human health. Rain gardens capture 100% of rainfall on the site, up to 4,000 cubic feet at a time, equivalent to 256 bathtubs of water.
+
+                Learn more about the sustainability effort at usbg.gov/bartholdi-fountain-and-gardens
+
+                https://www.nps.gov/findapark/index.html
+            """, style={"white-space": "pre-line"}
+)
+    ],
+    is_open=False,
+    id="description-modal"
+)
+
+@app.callback(
+    Output("hint-modal", "is_open"),
+    Input("hint-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def open_hint(n):
+    if n:
+        return True
+    return False
+
+@app.callback(
+    Output("description-modal", "is_open"),
+    Input("description-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def open_description(n):
+    if n:
+        return True
+    return False
+
+@app.callback(
+    Output("message-modal", "is_open"),
+    Input("message-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def open_message(n):
+    if n:
+        return True
+    return False
 
 app.layout = html.Div([
     header_img,
@@ -54,8 +121,11 @@ app.layout = html.Div([
     header,
     cache_info,
     hint_message,
+    hint_modal,
+    message_modal,
     description,
-    cache_bottom
+    description_modal,
+    cache_bottom,
 ])
 
 
