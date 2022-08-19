1d9f82b96c0b021e7ef6783c1d977eebb7ae2ded
nc2 <circlethewagons21@protonmail.com>
2022-08-18 23:14:37 -0400

    Create initial cache page

--- app.py
+++ app.py
@@ -4,17 +4,38 @@ from dash import html, dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 import geocoder
+
 g = geocoder.ip('me')
 
-print(g.latlng)
-
-dash_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+dash_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SIMPLEX])
 app = dash_app.server
 
+
+header_path = 'assets/geocache_header.png'
+header_img = html.Img(src=header_path, className="fullscreen")
+
+map_path = 'assets/bartholdi_map.png'
+map_img = html.Img(src=map_path, className="fullscreen")
+title = html.H1("Bartholdi's Bounty")
+subtitle = html.P("GCHU783H - Traditional")
+header = html.Div([title, subtitle], className="centered", style={"padding-top":"30px"})
+cache_info_path = 'assets/cache_info.png'
+cache_info = html.Img(src=cache_info_path, className="fullscreen")
+
+main_buttons = html.Div([dbc.Button("Navigate", className="button-style"), dbc.Button("Log", className="button-style")], className="centered")
+
+
+
 dash_app.layout = html.Div([
-    html.H1("Geocache Page"),
-    html.H2(g.latlng)
+    header_img,
+    map_img,
+    header,
+    cache_info
 ])
 
+
+
+
+
 if __name__ == '__main__':
-    dash_app.run_server(debug=True)
+    dash_app.run_server(debug=True, )
